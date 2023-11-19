b66eb15c9844655bb6d2f6649792a181d6862589
Maximilian Petkov <max@stan.vision>
2023-11-19 22:44:45 +0200

    first-commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.min.js";
-
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 function Sketch() {
     this.canvas = document.getElementById("container").firstElementChild;
+    this.imgs = document.querySelectorAll("img");
+    this.loadedImages = [];
     this.scene = null;
     this.camera = null;
     this.geometry = null;
     this.material = null;
     this.renderer = null;
     this.mesh = null;
+    this.controls = null;
+    this.raycaster = new THREE.Raycaster();
+    this.pointer = new THREE.Vector2();
     this.size = {
         width: window.innerWidth,
         height: window.innerHeight
     }
+    this.lenis = new Lenis({duration: 0.9});
 }
 
 Sketch.prototype.createScene = function() {
@@ -20,14 +26,29 @@ Sketch.prototype.createScene = function() {
 }
 
 Sketch.prototype.createCamera = function() {
-    this.camera = new THREE.PerspectiveCamera(70, this.size.width / this.size.height);
-    this.camera.position.z = 1;
+    // Calculating Height of viewport of THREEJS (units) to be EQUAL to the height of browser (pixels)
+    // The idea is to connect the two worlds (THREEJS and Browser) and units to be equl to pixels
+    const dist = 1;
+    const fov = 2 * Math.atan((this.size.height / 2) / dist) * (180 / Math.PI);
+    this.camera = new THREE.PerspectiveCamera(fov, this.size.width / this.size.height);
+    this.camera.position.z = dist;
 }
 
 Sketch.prototype.createModel = function() {
-    this.geometry = new THREE.PlaneGeometry(1, 1);
-    this.material = new THREE.MeshBasicMaterial( {
-        color: 0xffff00, 
+    /* 
+        Field of View is calculated in createCamera() method
+        That way the geometry size will be easily sized via pixels
+        Size of the geometry is in units and based of FOV now size will be in pixels
+        
+        The idea is to connect the two worlds (THREEJS and Browser) and units to be equl to pixels
+    */
+    this.geometry = new THREE.PlaneGeometry(343, 560, 25, 25);
+    this.material = new THREE.ShaderMaterial( {
+        uniforms: {
+            time: {value: 0}
+        },
+        fragmentShader: this.fragment(),
+        vertexShader: this.vertex(),
         side: THREE.DoubleSide,
         wireframe: true,
     });
@@ -39,6 +60,7 @@ Sketch.prototype.createRenderer = function() {
     this.renderer = new THREE.WebGLRenderer({
         canvas: this.canvas,
         antialias: true,
+        alpha: true,
     });
     this.renderer.setSize(this.size.width, this.size.height);
     this.renderer.render(this.scene, this.camera);
@@ -60,12 +82,223 @@ Sketch.prototype.resize = function() {
     }.bind(this));
 }
 
+Sketch.prototype.createControls = function() {
+    this.controls = new OrbitControls(this.camera, this.canvas);
+}
+
+Sketch.prototype.animate = function() {
+    gsap.ticker.add((time) => {
+        this.lenis.raf(time * 1000);
+        this.updateImagesPosition();
+        this.loadedImages.forEach(img => img.material.uniforms.uTime.value = time);
+        this.renderer.render(this.scene, this.camera);
+    });
+}
+
+Sketch.prototype.fragment = function() {
+    return `
+        uniform float uTime;
+        uniform sampler2D uImage;
+        varying vec2 vUv;
+
+        void main() {
+            vec4 img = texture2D(uImage, vUv);
+            gl_FragColor = img;
+        }
+    `;
+}
+
+Sketch.prototype.vertex = function() {
+    return `
+        //	Classic Perlin 3D Noise 
+        //	by Stefan Gustavson
+        //
+        vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+        vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+        vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+        
+        float cnoise(vec3 P){
+            vec3 Pi0 = floor(P); // Integer part for indexing
+            vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+            Pi0 = mod(Pi0, 289.0);
+            Pi1 = mod(Pi1, 289.0);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+            
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+            
+            vec4 gx0 = ixy0 / 7.0;
+            vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+            
+            vec4 gx1 = ixy1 / 7.0;
+            vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+            
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+            
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+            
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+            
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+        }
+
+        varying vec2 vUv;
+        
+        uniform float uTime;
+        uniform vec2 uHover;
+        uniform float uHoverState;
+
+        void main() {
+            vec3 newPosition = position;
+            float dist = distance(uv,uHover);
+            vUv = uv;
+
+            newPosition.z += uHoverState * (0.015 * sin(dist*10. + (uTime*1.1)));
+
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.); 
+        }
+    `;
+}
+
+Sketch.prototype.createImages = function() {
+    this.imgs.forEach((img, i) => {
+        const bounds = img.getBoundingClientRect();
+        const geometry = new THREE.PlaneGeometry(bounds.width, bounds.height, 25, 25);
+        const texture = new THREE.TextureLoader().load(img.src);
+        const material = new THREE.ShaderMaterial({
+            uniforms: {
+                uTime: {value: 0},
+                uImage: {value: texture},
+                uHover: {value: new THREE.Vector2(0.5, 0.5)}, // vec2 value will change from mouse move
+                uHoverState: {value: 0}, // Value is used to detect when is hover over image and from there to start the animation
+            },
+            fragmentShader: this.fragment(),
+            vertexShader: this.vertex(),
+            // wireframe: true,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.y = -bounds.top + (this.size.height/2) - (bounds.height / 2);
+        mesh.position.x = bounds.left - (this.size.width / 2) + (bounds.width/2);
+
+        this.scene.add(mesh);
+
+        this.loadedImages.push({
+            img: img,
+            top: bounds.top,
+            left: bounds.left,
+            width: bounds.width,
+            height: bounds.height,
+            material: material,
+            mesh: mesh,
+        });
+    });
+}
+
+Sketch.prototype.hasLoaded = function(cb) {
+    // Need to check Promises
+    const fontOpen = new Promise(resolve => new FontFaceObserver("Open Sans").load().then(() => resolve()));
+    const fontPlayfair = new Promise(resolve => new FontFaceObserver("Playfair Display").load().then(() => resolve()));
+    const preloadImages = new Promise((resolve, reject) => imagesLoaded(this.imgs, {background: true}, resolve));
+
+    Promise.all([fontOpen, fontPlayfair, preloadImages])
+    .then(() => cb())
+    .catch(err => console.log(err));
+}
+
+Sketch.prototype.hideHTMLImages = function() {
+    if(!this.loadedImages.length) return;
+    this.loadedImages.forEach(obj => obj.img.style.opacity = 0);
+}
+
+Sketch.prototype.updateImagesPosition = function() {
+    this.loadedImages.forEach(obj => {
+        obj.mesh.position.y = window.scrollY -obj.top + (this.size.height/2) - (obj.height / 2);
+        obj.mesh.position.x = obj.left - (this.size.width / 2) + (obj.width/2);
+    });
+}
+
+Sketch.prototype.mouseMove = function() {
+    window.addEventListener("mousemove", function(e) {
+        this.pointer.x = ( e.clientX / this.size.width ) * 2 - 1;
+        this.pointer.y = - ( e.clientY / this.size.height ) * 2 + 1;
+
+        this.raycaster.setFromCamera( this.pointer, this.camera );
+
+        const intersects = this.raycaster.intersectObjects( this.scene.children );
+
+        if(intersects.length) intersects[0].object.material.uniforms.uHover.value = intersects[0].uv;
+        
+    }.bind(this));
+}
+
+Sketch.prototype.hoverImage = function() {
+    this.loadedImages.forEach(obj => {
+        obj.img.addEventListener("mouseenter", () => gsap.to(obj.material.uniforms.uHoverState, { value: 1 }));
+        obj.img.addEventListener("mouseleave", () => gsap.to(obj.material.uniforms.uHoverState, { value: 0}));
+    });
+}
+
+Sketch.prototype.initLenis = function(time) {
+    this.lenis.raf(time);
+    requestAnimationFrame(() => this.initLenis(time));
+}
+
 Sketch.prototype.init = function() {
-    this.createScene();
-    this.createCamera();
-    this.createModel();
-    this.createRenderer();
-    this.resize();
+    this.hasLoaded(function(){
+        this.createScene();
+        this.createCamera();
+        this.createImages();
+        this.createRenderer();
+        this.createControls();
+        this.resize();
+        this.mouseMove();
+        this.hoverImage();
+        this.animate();
+        this.hideHTMLImages();
+    }.bind(this));
 }
 
 new Sketch().init();
